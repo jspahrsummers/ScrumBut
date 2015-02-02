a1c8fdce5fe130baf74c751108b8e215f9ba6299
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2015-02-02 14:05:40 -0800

    Refactor routes so repos list is main page

--- Application.hs
+++ Application.hs
@@ -32,7 +32,6 @@ import System.Log.FastLogger                (defaultBufSize, newStdoutLoggerSet,
 -- Don't forget to add new modules to your cabal file!
 import Handler.Common
 import Handler.Home
-import Handler.Repositories
 import Handler.Repo
 
 -- This line actually creates our YesodDispatch instance. It is the second half
