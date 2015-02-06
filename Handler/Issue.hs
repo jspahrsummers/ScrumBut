60f18917477454d26b031e5eec89041634ab3969
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2015-02-06 13:59:34 -0800

    Add really dumb estimate submission

--- Handler/Issue.hs
+++ Handler/Issue.hs
@@ -7,6 +7,14 @@ import qualified GitHub as GH
 data Points = One | Two | Three | Five | Eight | Thirteen
     deriving (Eq, Show, Ord)
 
+pointsToInt :: Points -> Int
+pointsToInt One = 1
+pointsToInt Two = 2
+pointsToInt Three = 3
+pointsToInt Five = 5
+pointsToInt Eight = 8
+pointsToInt Thirteen = 13
+
 data EstimateSubmission = EstimateSubmission
     { points :: Points
     } deriving Show
@@ -22,11 +30,29 @@ getIssueR ownerLogin name issueNumber = do
     repo <- GH.fetchRepo client ownerLogin name
     issue <- GH.fetchIssue client repo issueNumber
 
-    (formWidget, enctype) <- generateFormPost estimateForm
+    currentEstimate <- runDB $ do
+        issueId <- generateIssueId repo issue
+        getBy $ UniqueEstimate issueId userId
 
+    (formWidget, enctype) <- generateFormPost estimateForm
     defaultLayout $ do
         setTitle "ScrumBut | Issue"
         $(widgetFile "issue")
+
+generateIssueId repo issue = do
+    dbRepo <- upsert (Repository
+        { repositoryGithubId = show $ GH.repoId repo
+        , repositoryName = GH.repoName repo
+        , repositoryOwnerLogin = GH.userLogin $ GH.repoOwner repo
+        }) []
+
+    dbIssue <- upsert (Issue
+        { issueGithubId = show $ GH.issueId issue
+        , issueNumber = show $ GH.issueNumber issue
+        , issueRepositoryId = entityKey dbRepo
+        }) []
+
+    return $ entityKey dbIssue
 
 estimateAForm :: AForm Handler EstimateSubmission
 estimateAForm =
@@ -46,11 +72,27 @@ estimateForm = renderDivs estimateAForm
 
 -- TODO: This should really be PUT.
 postIssueR :: Text -> Text -> Integer -> Handler Html
-postIssueR ownerLogin name issueId = do
-    ((result, widget), enctype) <- runFormPost estimateForm
+postIssueR ownerLogin name issueNumber = do
+    ((result, _), _) <- runFormPost estimateForm
+
+    -- TODO: Reduce this duplication.
+    userId <- requireAuthId
+    user <- runDB $ get userId
+
+    let token = userToken $ fromJust user
+    client <- GH.newClient token
+
+    repo <- GH.fetchRepo client ownerLogin name
+    issue <- GH.fetchIssue client repo issueNumber
 
     case result of
-        FormSuccess submission -> return ()
+        FormSuccess submission -> runDB $ do
+            issueId <- generateIssueId repo issue
+            void $ upsert (Estimate
+                { estimateIssueId = issueId
+                , estimateUserId = userId
+                , estimatePoints = pointsToInt $ points submission
+                }) []
         _ -> setMessage "Error in form submission"
 
-    getIssueR ownerLogin name issueId
+    getIssueR ownerLogin name issueNumber
